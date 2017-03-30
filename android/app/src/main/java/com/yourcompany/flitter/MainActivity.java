8c1e4f4badc98215094388b61e6517a63bbfbed6
Kleak <segaud.kevin@gmail.com>
2017-03-30 11:23:48 +0200

    first commit

---
+++ android/app/src/main/java/com/yourcompany/flitter/MainActivity.java
@@ -0,0 +1,12 @@
+package com.yourcompany.flitter;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
+
