76edd75740b888a4df3a8c4f9d1df4272631ac76
Kleak <segaud.kevin@gmail.com>
2017-04-06 15:28:19 +0200

    no more chrome in the sign in process

--- lib/services/oauth/src/server.dart
+++ lib/services/oauth/src/server.dart
@@ -3,6 +3,22 @@ library oauth.server;
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/services.dart';
+
+Future<String> getContent() async {
+  String content = await rootBundle.loadString("assets/html/success.html");
+  if (content.isNotEmpty) {
+    return content;
+  }
+  return """
+    <html>
+      <body>
+        <h1>You can now close this page !</h1>
+      </body>
+    </html>
+    """;
+}
+
 Future<Stream<String>> server() async {
   final StreamController<String> onCode = new StreamController();
   HttpServer server =
@@ -12,7 +28,7 @@ Future<Stream<String>> server() async {
     request.response
       ..statusCode = 200
       ..headers.set("Content-Type", ContentType.HTML.mimeType)
-      ..write("<html><h1>You can now close this window</h1></html>");
+      ..write(await getContent());
     await request.response.close();
     await server.close(force: true);
     onCode.add(code);
