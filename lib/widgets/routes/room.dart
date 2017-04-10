fbc8aa06e52cf154c4b8618613524f4f6102ec2a
Kleak <segaud.kevin@gmail.com>
2017-04-11 00:53:48 +0200

    remove commented code + refactor listener

--- lib/widgets/routes/room.dart
+++ lib/widgets/routes/room.dart
@@ -28,24 +28,26 @@ class _RoomViewState extends State<RoomView> {
     _counter = 0;
     messages = [];
     _m = [];
-    config.api.room.onMessage.listen((Message m) {
-      if (_skip == 0) {
-        setState(() {
-          messages.add(m);
-        });
-      } else {
-        if (_counter < 49) {
-          _m.add(m);
-        } else {
-          _m.addAll(messages);
-          setState(() {
-            messages = _m;
-          });
-        }
-        _counter++;
-      }
-    });
+    config.api.room.onMessage.listen(_onMessage);
     config.api.room.messagesFromRoomId(config.room.id, skip: _skip);
+  }
+
+  void _onMessage(Message m) {
+    if (_skip == 0) {
+      setState(() {
+        messages.add(m);
+      });
+    } else {
+      if (_counter < 49) {
+        _m.add(m);
+      } else {
+        _m.addAll(messages);
+        setState(() {
+          messages = _m;
+        });
+      }
+      _counter++;
+    }
   }
 
   Future<Null> fetchData() async {
@@ -64,7 +66,6 @@ class _RoomViewState extends State<RoomView> {
         ),
       );
     }
-
     Widget body;
     if (messages.isEmpty) {
       body = new Center(child: new CircularProgressIndicator());
@@ -74,37 +75,6 @@ class _RoomViewState extends State<RoomView> {
       chatRoom.onNeedData.listen((_) => fetchData());
       body = chatRoom;
     }
-
-//    Widget body = new StreamBuilder<Message>(
-//      stream: config.api.room.onMessage,
-//      builder: (BuildContext context, AsyncSnapshot<Message> snapshot) {
-//        if (snapshot.connectionState == ConnectionState.waiting) {
-//          return new Center(child: new CircularProgressIndicator());
-//
-//        }
-//        Message m = snapshot.data;
-//        print("build message: $m");
-//        messages.add(m);
-//        print("build messages: $messages");
-//        final ChatRoomWidget roomWidget =
-//            new ChatRoomWidget(messages: messages.reversed.toList());
-//        roomWidget.onNeedData.listen((_) => fetchData());
-//        return roomWidget;
-//      },
-//    );
-//    Widget body = new FutureBuilder<List<Message>>(
-//      future: config.api.room.messagesFromRoomId(config.room.id, skip: _skip),
-//      builder: (BuildContext context, AsyncSnapshot<List<Message>> snapshot) {
-//        if (snapshot.connectionState != ConnectionState.done) {
-//          return new Center(child: new CircularProgressIndicator());
-//        }
-//        messages = snapshot.data;
-//        final ChatRoomWidget roomWidget =
-//            new ChatRoomWidget(messages: messages.reversed.toList());
-//        roomWidget.onNeedData.listen((_) => fetchData());
-//        return roomWidget;
-//      },
-//    );
     return new Scaffold(
       appBar: new AppBar(title: new Text(config.room.name)),
       body: body,
