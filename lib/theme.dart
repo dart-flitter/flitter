56483af4106ccdc35cdb8d15ab027d67f161d83d
Hadrien Lejard <hadrien.lejard@gmail.com>
2017-04-15 20:30:17 +0200

    change colors

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
-ThemeData kTheme = new ThemeData(primarySwatch: Colors.pink);
+ThemeData kTheme = new ThemeData(
+    primarySwatch: Colors.indigo,
+    accentColor: Colors.pink[500],
+    primaryColor: Colors.indigo[800],
+);
